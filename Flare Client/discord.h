bd1d1369a988f1affb19b16c4148c48512c1724b
DisabledMallis <mallisiscool@gmail.com>
2019-12-23 19:31:35 -0500

    Fix revert

--- Flare Client/discord.h
+++ Flare Client/discord.h
@@ -1,6 +1,6 @@
 #pragma once
-#include <discord_register.h>
-#include <discord_rpc.h>
+#include "Discord/discord_register.h"
+#include "Discord/discord_rpc.h"
 #include <Windows.h>
 #include <chrono>
 
