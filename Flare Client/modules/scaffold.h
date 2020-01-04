9f6ee73e35dcf5c7c84b0bc957ecbf4982fc271f
EchoHackCmd <33047114+EchoHackCmd@users.noreply.github.com>
2020-01-04 22:51:40 +0000

    Scaffold rewrite
    Scaffold rewrite

--- Flare Client/modules/scaffold.h
+++ Flare Client/modules/scaffold.h
@@ -1,9 +1,13 @@
 #pragma once
 #include "../LittleHacker.h"
 #include "ModuleHandler.h"
+#include <vector>
 
 class Scaffold {
 public:
 	Scaffold(HANDLE hProcess, int toggle);
+	static void SpecifyBlockFace(int blockSide);
+	static void SpecifiyBlockPosition(int X, int Y, int Z);
+	static void AdditionalCalculations(float X, float Y, float Z, float playerYaw);
 };
 
