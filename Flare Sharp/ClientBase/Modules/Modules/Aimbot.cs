a3156c6f132ffa85fb0a1dbc6d5e5c9f658e0963
EchoHackCmd <33047114+EchoHackCmd@users.noreply.github.com>
2020-01-26 06:56:07 +0000

    Added Aimbot Calculations
    Added Aimbot Calculations

--- Flare Sharp/ClientBase/Modules/Modules/Aimbot.cs
+++ Flare Sharp/ClientBase/Modules/Modules/Aimbot.cs
@@ -15,45 +15,52 @@ namespace Flare_Sharp.ClientBase.Modules.Modules
         {
         }
 
+        public static List<float> getCalculationsToPos(float[] localPos, float[] targetPos)
+        {
+            List<float> calculations = new List<float>();
+
+            float dX = localPos[0] - targetPos[0];
+            float dY = localPos[1] - targetPos[1];
+            float dZ = localPos[2] - targetPos[2];
+            double distance = Math.Sqrt(dX * dX + dY * dY + dZ * dZ);
+            float pitch = ((float)Math.Atan2(dY, (float)distance) * (float)3.13810205 / (float)3.141592653589793);
+            float yaw = ((float)Math.Atan2(dZ, dX) * (float)3.1381025 / (float)3.141592653589793) + (float)-1.569051027;
+            calculations.Add(-pitch);
+            calculations.Add(-yaw);
+            return calculations;
+        }
+
         public override void onTick()
         {
             base.onTick();
             List<Entity> Entity = EntityList.getEntityList();
             List<float> distances = new List<float>();
             float playerX, playerY, playerZ;
-            playerX = SDK.instance.player.currentX1;
-            playerY = SDK.instance.player.currentY1;
-            playerZ = SDK.instance.player.currentZ1;
+            float[] localPosition = { SDK.instance.player.currentX1, SDK.instance.player.currentY1, SDK.instance.player.currentZ1 };
             foreach(Entity e in Entity)
             {
-                float entityX = e.currentX1;
-                float entityY = e.currentY1;
-                float entityZ = e.currentZ1;
-                float dX = playerX - entityX;
-                float dY = playerY - entityY;
-                float dZ = playerZ - entityZ;
-                double distance = Math.Sqrt(dX * dX + dY * dY + dZ * dZ);
+                float[] targetPosition = { e.currentX1, e.currentY1, e.currentZ1 };
+                float dX = localPosition[0] - targetPosition[0];
+                float dZ = localPosition[2] - targetPosition[2];
+                double distance = Math.Sqrt(dX * dX + dZ * dZ);
                 if (distance <= 12)
                 {
                     distances.Add((float)distance);
-                    Console.WriteLine("Added");
                 }
             }
-            distances.Sort();
             if(distances.Count() > 0)
             {
+                distances.Sort();
                 foreach (Entity e in Entity)
                 {
-                    float entityX = e.currentX1;
-                    float entityY = e.currentY1;
-                    float entityZ = e.currentZ1;
-                    float dX = playerX - entityX;
-                    float dZ = playerZ - entityZ;
+                    float[] targetPosition = { e.currentX1, e.currentY1, e.currentZ1};
+                    float dX = localPosition[0] - targetPosition[0];
+                    float dZ = localPosition[0] - targetPosition[2];
                     double distance = Math.Sqrt(dX * dX + dZ * dZ);
-                    if ((float)distance == distances[0])
-                    {
-                        Console.WriteLine("!!!");
-                    }
+                    if (distances[0] == (float)distance) Console.WriteLine("!");
+                    List<float> staringPos = getCalculationsToPos(localPosition, targetPosition);
+                    MCM.writeFloat(Pointers.mousePitch(), staringPos[0]);
+                    MCM.writeFloat(Pointers.mouseYaw(), staringPos[1]);
                 }
             }
         }
