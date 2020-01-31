f1d41fb7c0820b910ff9a42eadb9d2642f92d184
EchoHackCmd <33047114+EchoHackCmd@users.noreply.github.com>
2020-01-31 23:53:08 +0000

    Jetpack (Only has base)
    Jetpack (Only has base)

---
+++ Flare Sharp/ClientBase/Modules/Modules/Jetpack.cs
@@ -0,0 +1,29 @@
+﻿using Flare_Sharp.ClientBase.Categories;
+using Flare_Sharp.ClientBase.Keybinds;
+using Flare_Sharp.Memory;
+using Flare_Sharp.Memory.CraftSDK;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flare_Sharp.ClientBase.Modules.Modules
+{
+    public class Jetpack : Module
+    {
+        public Jetpack() : base("Jetpack", CategoryHandler.registry.categories[1], '-', true)
+        {
+            KeybindHandler.clientKeyHeldEvent += OnKeyHeld;
+        }
+        public void OnKeyHeld(object sender, clientKeyEvent e)
+        {
+            if (e.key == 'F') enabled = true;
+        }
+        public override void onTick()
+        {
+            base.onTick();
+            if(!KeybindHandler.isKeyDown('F')) enabled = false;
+        }
+    }
+}
