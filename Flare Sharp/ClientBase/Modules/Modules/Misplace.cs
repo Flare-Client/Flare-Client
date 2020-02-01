a6d566c6e773208cb10bb85ef4e89fa582261d1a
DisabledMallis <mallisiscool@gmail.com>
2020-01-31 21:50:54 -0500

    improve misplace

--- Flare Sharp/ClientBase/Modules/Modules/Misplace.cs
+++ Flare Sharp/ClientBase/Modules/Modules/Misplace.cs
@@ -20,12 +20,30 @@ namespace Flare_Sharp.ClientBase.Modules.Modules
             base.onTick();
             foreach (Entity e in EntityList.getEntityList())
             {
+                if (e.distanceTo(SDK.instance.player) < 2)
+                {
+                    continue;
+                }
                 if (e.distanceTo(SDK.instance.player) <= 12) {
                     float lpX = SDK.instance.player.X1;
                     float lpY = SDK.instance.player.Y1;
                     float lpZ = SDK.instance.player.Z1;
 
-                    e.teleportE(, , );
+                    Console.WriteLine("Player:    " + lpX + ", " + lpY + ", " + lpZ);
+
+                    float eX = e.currentX3;
+                    float eY = e.currentY3;
+                    float eZ = e.currentZ3;
+
+                    Console.WriteLine("Target:    " + eX + ", " + eY + ", " + eZ);
+
+                    float mpX = (lpX + eX) / 2;
+                    float mpZ = (lpZ + eZ) / 2;
+
+                    Console.WriteLine("ToPos:    " + mpX + ", " + eY + ", " + mpZ);
+
+                    Console.WriteLine("TargetAddress: " + e.addr.ToString("X"));
+                    e.teleportE(mpX, lpY, mpZ);
                 }
             }
         }
