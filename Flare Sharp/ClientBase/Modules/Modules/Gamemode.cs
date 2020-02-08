1c869255c940882cf25681458ab63834336954f5
EchoHackCmd <33047114+EchoHackCmd@users.noreply.github.com>
2020-02-08 16:40:17 +0000

    Minor Changes
    Minor Changes

--- Flare Sharp/ClientBase/Modules/Modules/Gamemode.cs
+++ Flare Sharp/ClientBase/Modules/Modules/Gamemode.cs
@@ -15,6 +15,7 @@ namespace Flare_Sharp.ClientBase.Modules.Modules
         public int savedGamemode;
         public Gamemode() : base("Gamemode", CategoryHandler.registry.categories[2], (char)0x07, false)
         {
+            RegisterSliderSetting("Mode", 0, 1, 2);
         }
 
         public override void onEnable()
@@ -22,8 +23,8 @@ namespace Flare_Sharp.ClientBase.Modules.Modules
             base.onEnable();
             savedGamemode = SDK.instance.player.currentGamemode;
             inventoryState = SDK.instance.player.viewCreativeItems;
-            SDK.instance.player.currentGamemode = 1;
-            SDK.instance.player.viewCreativeItems = 1;
+            SDK.instance.player.currentGamemode = sliderSettings[0].value;
+            if(sliderSettings[0].value == 1) SDK.instance.player.viewCreativeItems = 1;
         }
 
         public override void onDisable()
