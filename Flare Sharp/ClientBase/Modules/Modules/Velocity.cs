bd1c9190dc988afc6ec6206834b2a893063485ec
EchoHackCmd <33047114+EchoHackCmd@users.noreply.github.com>
2020-02-11 04:58:12 +0000

    Added backwards left & right to Velocity
    Added backwards left & right to Velocity

--- Flare Sharp/ClientBase/Modules/Modules/Velocity.cs
+++ Flare Sharp/ClientBase/Modules/Modules/Velocity.cs
@@ -31,14 +31,12 @@ namespace Flare_Sharp.ClientBase.Modules.Modules
                 List<float> directionalVec = SDK.instance.directionalVector((SDK.instance.player.yaw + -90) * (float)Math.PI / 178F, (float)Math.PI / 178F);
                 SDK.instance.player.velX = (float)sliderSettings[0].value / 100F * directionalVec[0];
                 SDK.instance.player.velZ = (float)sliderSettings[0].value / 100F * directionalVec[2];
-            }
-            else if (!KeybindHandler.isKeyDown('W') && !KeybindHandler.isKeyDown('S') && KeybindHandler.isKeyDown('A') && !KeybindHandler.isKeyDown('D'))
+            } else if (!KeybindHandler.isKeyDown('W') && !KeybindHandler.isKeyDown('S') && KeybindHandler.isKeyDown('A') && !KeybindHandler.isKeyDown('D'))
             {
                 List<float> directionalVec = SDK.instance.directionalVector((SDK.instance.player.yaw) * (float)Math.PI / 178F, (float)Math.PI / 178F);
                 SDK.instance.player.velX = (float)sliderSettings[0].value / 100F * directionalVec[0];
                 SDK.instance.player.velZ = (float)sliderSettings[0].value / 100F * directionalVec[2];
-            }
-            else if (!KeybindHandler.isKeyDown('S') && !KeybindHandler.isKeyDown('W') && !KeybindHandler.isKeyDown('A') && KeybindHandler.isKeyDown('D'))
+            } else if (!KeybindHandler.isKeyDown('S') && !KeybindHandler.isKeyDown('W') && !KeybindHandler.isKeyDown('A') && KeybindHandler.isKeyDown('D'))
             {
                 List<float> directionalVec = SDK.instance.directionalVector((SDK.instance.player.yaw + 180) * (float)Math.PI / 178F, (float)Math.PI / 178F);
                 SDK.instance.player.velX = (float)sliderSettings[0].value / 100F * directionalVec[0];
@@ -51,6 +49,16 @@ namespace Flare_Sharp.ClientBase.Modules.Modules
             } else if(KeybindHandler.isKeyDown('W') && KeybindHandler.isKeyDown('D'))
             {
                 List<float> directionalVec = SDK.instance.directionalVector((SDK.instance.player.yaw + 110) * (float)Math.PI / 178F, (float)Math.PI / 178F);
+                SDK.instance.player.velX = (float)sliderSettings[0].value / 100F * directionalVec[0];
+                SDK.instance.player.velZ = (float)sliderSettings[0].value / 100F * directionalVec[2];
+            } else if(KeybindHandler.isKeyDown('S') && KeybindHandler.isKeyDown('D'))
+            {
+                List<float> directionalVec = SDK.instance.directionalVector((SDK.instance.player.yaw + -110) * (float)Math.PI / 178F, (float)Math.PI / 178F);
+                SDK.instance.player.velX = (float)sliderSettings[0].value / 100F * directionalVec[0];
+                SDK.instance.player.velZ = (float)sliderSettings[0].value / 100F * directionalVec[2];
+            } else if(KeybindHandler.isKeyDown('S') && KeybindHandler.isKeyDown('A'))
+            {
+                List<float> directionalVec = SDK.instance.directionalVector((SDK.instance.player.yaw + -70) * (float)Math.PI / 178F, (float)Math.PI / 178F);
                 SDK.instance.player.velX = (float)sliderSettings[0].value / 100F * directionalVec[0];
                 SDK.instance.player.velZ = (float)sliderSettings[0].value / 100F * directionalVec[2];
             }
