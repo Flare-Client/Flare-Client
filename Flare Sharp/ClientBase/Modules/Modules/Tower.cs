4ae71c3e48c9981353d0d0b75e869d904cbf3baa
EchoHackCmd <33047114+EchoHackCmd@users.noreply.github.com>
2020-02-19 07:25:34 +0000

    Tower improvements
    Tower improvements

--- Flare Sharp/ClientBase/Modules/Modules/Tower.cs
+++ Flare Sharp/ClientBase/Modules/Modules/Tower.cs
@@ -20,10 +20,10 @@ namespace Flare_Sharp.ClientBase.Modules.Modules
         {
             if (enabled && e.key == (char)0x02)
             {
-                if(SDK.instance.player.pitch >= 72F && SDK.instance.player.heldItemCount > 0 && Pointers.blockPosY > 0 && Pointers.blockPosY <= 256)
+                if(SDK.instance.player.pitch >= 85F && SDK.instance.player.heldItemCount > 0 && Pointers.blockPosY > 0 && Pointers.blockPosY <= 256)
                 {
                     Pointers.blockSide = 1;
-                    SDK.instance.player.velY = 0.3F;
+                    SDK.instance.player.velY = 0.5F;
                 }
             }
         }
