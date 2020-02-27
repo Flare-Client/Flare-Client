b9d6ba44e44e6caf8204abbe13604eaaf6379b22
EchoHackCmd <33047114+EchoHackCmd@users.noreply.github.com>
2020-02-27 23:32:05 +0000

    Fixed a crash when leaving the game
    Fixed a crash when leaving the game

--- Flare Sharp/ClientBase/Modules/Modules/Scaffold.cs
+++ Flare Sharp/ClientBase/Modules/Modules/Scaffold.cs
@@ -39,7 +39,7 @@ namespace Flare_Sharp.ClientBase.Modules.Modules
         public override void onDisable()
         {
             base.onDisable();
-            MCM.writeBaseByte(Pointers.blockFace, 8);
+            MCM.writeBaseByte(Pointers.blockFace, 84);
             MCM.writeBaseByte(Pointers.rapidPlace, 0);
         }
     }
