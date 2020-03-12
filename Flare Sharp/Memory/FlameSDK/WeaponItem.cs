aafd47b069ba577e947a28ef08373de4d1b0c0d4
DisabledMallis <mallisiscool@gmail.com>
2020-03-11 22:01:58 -0400

    start item stuffs

---
+++ Flare Sharp/Memory/FlameSDK/WeaponItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flare_Sharp.Memory.FlameSDK
+{
+    public class WeaponItem : Item
+    {
+        public WeaponItem(ulong addr) : base(addr) { }
+        public int attackDamage
+        {
+            get
+            {
+                MCM.readInt(addr + 0x1C0);
+            }
+            set
+            {
+                MCM.writeInt(addr + 0x1C0, value);
+            }
+        }
+    }
+}
