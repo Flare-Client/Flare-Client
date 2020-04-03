c4830636f5680b636681e437d6bc4d7552c2677d
EchoHackCmd <33047114+EchoHackCmd@users.noreply.github.com>
2020-04-03 01:24:43 +0000

    NoFall now uses the gamerule class
    NoFall now uses the gamerule class

--- Flare Sharp/ClientBase/Modules/Modules/NoFall.cs
+++ Flare Sharp/ClientBase/Modules/Modules/NoFall.cs
@@ -1,17 +1,31 @@
 ﻿using Flare_Sharp.ClientBase.Categories;
 using Flare_Sharp.Memory.FlameSDK;
+using System;
 
 namespace Flare_Sharp.ClientBase.Modules.Modules
 {
     public class NoFall : Module
     {
+        public static bool savedNoFallRule;
         public NoFall() : base("NoFall", CategoryHandler.registry.categories[2], (char)0x07, false)
         {
+        }
+        public override void onEnable()
+        {
+            base.onEnable();
+            savedNoFallRule = Minecraft.clientInstance.localPlayer.level.gamerules[9].toggle;
+            Console.WriteLine(Minecraft.clientInstance.localPlayer.level.gamerules[9].toggle);
         }
         public override void onTick()
         {
             base.onTick();
-            Minecraft.clientInstance.localPlayer.isFalling = 0;
+            Minecraft.clientInstance.localPlayer.level.gamerules[9].toggle = false;
+            Console.WriteLine(Minecraft.clientInstance.localPlayer.level.gamerules[9].toggle);
+        }
+        public override void onDisable()
+        {
+            base.onDisable();
+            Minecraft.clientInstance.localPlayer.level.gamerules[9].toggle = savedNoFallRule;
         }
     }
 }
