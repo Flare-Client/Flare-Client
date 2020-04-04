e2b8343aa546d8d970a4f3ae89304bb9c46604b3
DisabledMallis <mallisiscool@gmail.com>
2020-04-04 07:37:33 -0400

    fix some stuff

--- Flare Sharp/ClientBase/IO/ProfileIO.cs
+++ Flare Sharp/ClientBase/IO/ProfileIO.cs
@@ -10,35 +10,78 @@ namespace Flare_Sharp.ClientBase.IO
 {
     public class ProfileIO
     {
+        static string currentProfile = "DefaultProfile";
         static string configDir = Environment.CurrentDirectory + "/FlareData/";
-        public static void saveSetting<T>(string profile, string path, T value)
+        public static void saveSetting<T>(string path, T value)
         {
-            string file = configDir + profile + ".fdf";
-            if (!File.Exists(file)){
-                File.Create(file);
-            }
-            string[] lines = File.ReadAllLines(file);
-            List<string> newLines = new List<string>();
-            foreach (string line in lines)
+            saveSetting<T>(currentProfile, path, value);
+        }
+        public static bool saveSetting<T>(string profile, string path, T value)
+        {
+            try
             {
-                if (line.StartsWith(path + ":"))
+                //Check if required files exist
+                if (!Directory.Exists(configDir))
                 {
-                    BinaryFormatter bin = new BinaryFormatter();
-                    string newLine = path+":";
-                    MemoryStream serializationStream = new MemoryStream();
-                    bin.Serialize(serializationStream, value);
-                    newLine += Encoding.UTF8.GetString(serializationStream.ToArray());
+                    Directory.CreateDirectory(configDir);
+                }
+                string file = configDir + profile + ".fdf";
+                if (!File.Exists(file))
+                {
+                    File.Create(file).Close();
+                }
+
+                //Create string of serialized object
+                BinaryFormatter bin = new BinaryFormatter();
+                string newLine = path + ":";
+                MemoryStream serializationStream = new MemoryStream();
+                bin.Serialize(serializationStream, value);
+                newLine += Encoding.UTF8.GetString(serializationStream.ToArray());
+
+                //Read file to replace it if it exists
+                string[] lines = File.ReadAllLines(file);
+                List<string> newLines = new List<string>();
+                bool appendRequired = true;
+                foreach (string line in lines)
+                {
+                    if (line.StartsWith(path))
+                    {
+                        //the line does exist so we replace it
+                        newLines.Add(newLine);
+                        appendRequired = false;
+                        continue;
+                    }
+                    newLines.Add(line);
+                }
+                //the line doesnt exist so we have to append it
+                if (appendRequired)
+                {
                     newLines.Add(newLine);
                 }
-                newLines.Add(line);
+                File.WriteAllLines(file, newLines);
+                return true;
             }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+                return false;
+            }
+        }
+        public static T loadSetting<T>(string path, out bool success)
+        {
+            return loadSetting<T>(currentProfile, path, out success);
         }
         public static T loadSetting<T>(string profile, string path, out bool success)
         {
+            if (!Directory.Exists(configDir))
+            {
+                Directory.CreateDirectory(configDir);
+            }
             string file = configDir + profile + ".fdf";
             if (!File.Exists(file))
             {
-                File.Create(file);
+                File.Create(file).Close();
             }
             string[] lines= File.ReadAllLines(file);
             string targetLine = "";
