b516cdc319af50e6564a5b4e408826f55d88725c
EchoHackCmd <33047114+EchoHackCmd@users.noreply.github.com>
2020-04-18 00:19:36 +0100

    AutoSprint

--- Flare Sharp/ClientBase/Modules/Modules/AutoSprint.cs
+++ Flare Sharp/ClientBase/Modules/Modules/AutoSprint.cs
@@ -19,7 +19,7 @@ namespace Flare_Sharp.ClientBase.Modules.Modules
         public override void onDisable()
         {
             base.onDisable();
-            byte[] write = { 0x44,0x0F,0x2F,0x66,0x0C };
+            byte[] write = { 0x44,0x0F,0x2F,0x66,0x0C }; 
             MCM.writeBaseBytes(Statics.autoSprint, write);
         }
     }
--- Flare Sharp/Memory/FlameSDK/Statics.cs
+++ Flare Sharp/Memory/FlameSDK/Statics.cs
@@ -8,10 +8,10 @@ namespace Flare_Sharp.Memory.FlameSDK
 {
     public class Statics
     {
-        public static int attackSwing = 0x102E23E;
-        public static int handSwingPacket = 0x8AADBF;
+        public static int attackSwing = 0x10809BE; //v1.14.6
+        public static int handSwingPacket = 0x0;
         public static int rapidPlace = 0x1080940; //v1.14.6
-        public static int autoSprint = 0x1A652C0;
+        public static int autoSprint = 0x1ABC1E0; //v1.14.6
         public static int criticalsPacket = 0xFDBC66;
         public static int showCoordinates = 0x6029FD;
         public static int blockFace = 0x5D53C4;
@@ -26,7 +26,7 @@ namespace Flare_Sharp.Memory.FlameSDK
         public static int ladderUp = 0x13D7430;
         public static int ladderDown = 0x13CD065;
         public static int inWaterTick = 0x1222DAD;
-        public static int survivalReachCmp = 0x626171; //V1.14.6
+        public static int survivalReachCmp = 0x626171; //v1.14.6
         public static int blockBreak = 0x14A6125;
         public static int shadowRenderer = 0xA17A85;
     }
