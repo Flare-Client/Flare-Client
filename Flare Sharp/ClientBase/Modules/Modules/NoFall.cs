00e9f5d5b9d093a3780465d70357803edfe93e1e
EchoHackCmd <33047114+EchoHackCmd@users.noreply.github.com>
2020-04-18 07:36:40 +0100

    Update NoFall.cs

--- Flare Sharp/ClientBase/Modules/Modules/NoFall.cs
+++ Flare Sharp/ClientBase/Modules/Modules/NoFall.cs
@@ -6,26 +6,13 @@ namespace Flare_Sharp.ClientBase.Modules.Modules
 {
     public class NoFall : Module
     {
-        public static bool savedNoFallRule;
         public NoFall() : base("NoFall", CategoryHandler.registry.categories[2], (char)0x07, false)
         {
-        }
-        public override void onEnable()
-        {
-            base.onEnable();
-            savedNoFallRule = Minecraft.clientInstance.localPlayer.level.gamerules[9].toggle;
-            Console.WriteLine(Minecraft.clientInstance.localPlayer.level.gamerules[9].toggle);
         }
         public override void onTick()
         {
             base.onTick();
-            Minecraft.clientInstance.localPlayer.level.gamerules[9].toggle = false;
-            Console.WriteLine(Minecraft.clientInstance.localPlayer.level.gamerules[9].toggle);
-        }
-        public override void onDisable()
-        {
-            base.onDisable();
-            Minecraft.clientInstance.localPlayer.level.gamerules[9].toggle = savedNoFallRule;
+            Minecraft.clientInstance.localPlayer.isFalling = 0;
         }
     }
 }
