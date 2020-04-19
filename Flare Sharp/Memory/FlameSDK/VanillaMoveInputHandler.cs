738f2031d5d80f3e916918467eaf6bc014a4bdd6
EchoHackCmd <33047114+EchoHackCmd@users.noreply.github.com>
2020-04-19 10:19:34 +0100

    Added VanillaMoveInputHandler class

---
+++ Flare Sharp/Memory/FlameSDK/VanillaMoveInputHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flare_Sharp.Memory.FlameSDK
+{
+    public class VanillaMoveInputHandler : SDKObj
+    {
+        public VanillaMoveInputHandler(ulong addr) : base(addr)
+        {
+        }
+
+        public byte isCrouching
+        {
+            get
+            {
+                return MCM.readByte(addr + 0x48);
+            }
+            set
+            {
+                MCM.writeByte(addr + 0x40, value);
+            }
+        }
+    }
+}
