ae708452ad78f6845a37facff803792cf53e47f9
EchoHackCmd <33047114+EchoHackCmd@users.noreply.github.com>
2020-04-20 07:53:52 +0100

    Tag

--- Flare Sharp/Program.cs
+++ Flare Sharp/Program.cs
@@ -21,7 +21,7 @@ namespace Flare_Sharp
 {
     class Program
     {
-        public static string version = "0.0.7";
+        public static string version = "0.0.7.1";
         public static int threadSleep = 1;
         public static EventHandler<EventArgs> mainLoop;
         public static bool limitCpu = false;
