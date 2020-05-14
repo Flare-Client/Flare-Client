5e1178951c74fab74787370676c0ae071d4c12cb
Daxanius <38554396+Daxanius@users.noreply.github.com>
2020-05-14 20:44:03 +0200

    Update Freecam.cs
    - Fixed a bug that caused the player to spawn in the floor when disabling freecam.
    - Added Fly and Phase to freecam for more freedom
    - Improved the code layout tad

--- Flare Sharp/ClientBase/Modules/Modules/Freecam.cs
+++ Flare Sharp/ClientBase/Modules/Modules/Freecam.cs
@@ -20,7 +20,7 @@ namespace Flare_Sharp.ClientBase.Modules.Modules
         {
             base.onEnable();
             savedCoordinates.Add(Minecraft.clientInstance.localPlayer.currentX1);
-            savedCoordinates.Add((float)Math.Floor(Minecraft.clientInstance.localPlayer.currentY1 - 1));
+            savedCoordinates.Add((float)Math.Floor(Minecraft.clientInstance.localPlayer.currentY1));
             savedCoordinates.Add(Minecraft.clientInstance.localPlayer.currentZ1);
             savedPitchAndYaw.Add(Minecraft.clientInstance.firstPersonLookBehavior.cameraPitch);
             savedPitchAndYaw.Add(Minecraft.clientInstance.firstPersonLookBehavior.cameraYaw);
@@ -28,6 +28,7 @@ namespace Flare_Sharp.ClientBase.Modules.Modules
             byte[] write = { 0x90, 0x90, 0x90 };
             MCM.writeBaseBytes(Statics.movementPacket, write);
         }
+
         public override void onDisable()
         {
             base.onDisable();
@@ -54,8 +55,8 @@ namespace Flare_Sharp.ClientBase.Modules.Modules
         public override void onTick()
         {
             base.onTick();
-            Minecraft.clientInstance.localPlayer.Y2 = Minecraft.clientInstance.localPlayer.Y1;
             Minecraft.clientInstance.localPlayer.isFlying = 1;
+            Minecraft.clientInstance.localPlayer.Y2 = Minecraft.clientInstance.localPlayer.Y1;
         }
     }
 }
